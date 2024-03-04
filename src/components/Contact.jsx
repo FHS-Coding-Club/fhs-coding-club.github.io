6de3cb1e088650c6765013f45df21340f1fb364f
Rohan-Peddamallu <87048658+Rohan-Peddamallu@users.noreply.github.com>
2024-03-04 15:47:02 -0600

    Tried to add google form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,8 +4,33 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  function postToGoogle(formData) {
+    console.log(formData)
+    fetch("https://docs.google.com/forms/d/e/1FAIpQLSfAjbKeAjjlNA4Mm2mKYeBYmo4hi3uqjgTMHsMaSY3s5nAtpQ/formResponse", {
+      //add your google form generated numbers below which are also the 'names' of your inputs
+      mode: 'no-cors',     
+      data: formData,
+      method: "POST",
+      success: function (d) {
+        console.log(d)
+      },
+      error: function (x, y, z) {
+        console.log(x)
+      }
+    });
+    return false;
+  }
+    
   const containerVariants = {
     hidden: { opacity: 0, x: -100 },
     visible: {
@@ -28,19 +53,28 @@ const Contact = () => {
         <p className={styles.sectionSubText}>Fill Out This Form</p>
 
         <div className="mt-[6rem] ml-[5rem]">
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfAjbKeAjjlNA4Mm2mKYeBYmo4hi3uqjgTMHsMaSY3s5nAtpQ/viewform?usp=sf_link" target="_blank">
-            <iframe
-              src="https://docs.google.com/forms/d/e/1FAIpQLSfN9g0ue_xSZGmu-y_ghaBXFn9vmCueEV0hrULpbKEYeoqcSw/viewform?embedded=true"
-              width="700"
-              height="600"
-              frameborder="0"
-              marginheight="0"
-              marginwidth="0"
-              className="rounded-2xl sm:w-full w-[500px]"
-            >
-              Loading…
-            </iframe>
-          </a>
+          <form onSubmit={handleSubmit(postToGoogle)}>
+            <div className="form-control">
+              <label>First Name</label>
+              <input type="text" name="entry.539811825" {...register("entry.539811825", {required: true})} />
+            </div>
+            <div className="form-control">
+              <label>Last Name</label>
+              <input name="entry.1906472314" {...register("entry.1906472314", {required: true})} />
+            </div>
+            <div className="form-control">
+              <label>What project are you interested in working on?</label>
+              <input name="entry.1684295415" {...register("entry.1684295415", {required: true})} />
+            </div>
+            <div className="form-control">
+              <label>Email Address</label>
+              <input name="emailAddress" {...register("emailAddress", {required: true})} />
+            </div>
+            <div className="form-control">
+              <label></label>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
         </div>
       </motion.div>
     </div>
